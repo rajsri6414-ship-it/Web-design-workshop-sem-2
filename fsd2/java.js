1fcd4cfe006e2be7950572f16e1948610a3749bc
ASUS <asus@example.com>
2026-08-20 11:17:25 +0530

    add new file

---
+++ fsd2/java.js
@@ -0,0 +1,40 @@
+// let a = 5;
+// let b = 10;
+// let c = a + b;
+// console.log(c);
+// console.log(a==b);
+// console.log(a===b);
+// console.log(a>5&&b<15);
+
+// let marks =85;
+// if(marks>=90){
+//     console.log('A grade');
+// }else if(marks>=60){
+//     console.log('B grade');
+// } else {
+//     console.log('C grade');
+// }
+
+
+// for(let i=0; i<5; i++){
+//     console.log(i);
+// }
+// let nums = 0;
+// while(nums<=10){
+//     console.log(nums);
+//     nums += 2;
+    
+// }
+
+// const greet = (name = "guest") => "hello " + name;
+// console.log(greet());
+// console.log(greet("John"));
+ 
+// const number = [1,2,3,4,5];
+// const double = number.map((num) => num * 2);
+// const evens = number.filter((num) => num % 2 === 0);
+// const sum = number.reduce((acc, num) => acc + num, 0);
+// console.log(double);
+// console.log(evens);
+// console.log(sum);
+
